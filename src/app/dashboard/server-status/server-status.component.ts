2bf0d953f656c010fc8235845004d59915723b70
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-07-23 20:30:48 +0200

    Add console log

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -13,6 +13,7 @@ export class ServerStatusComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('ON INIT');
     setInterval(() => {
       const rnd = Math.random(); // 0 - 0.99999999999999
 
@@ -24,5 +25,9 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
   }
 }
