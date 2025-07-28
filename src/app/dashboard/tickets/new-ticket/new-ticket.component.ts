6aef8091b02a58be0e8cf511d901af92e0bb194e
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-07-28 21:19:05 +0200

    two way binding repetition

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, output, ViewChild } from '@angular/core';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -14,6 +15,8 @@ export class NewTicketComponent implements OnInit ,AfterViewInit {
   @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   // private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   // @Output() add = new EventEmitter({title: string; text: string});
+  enteredTitle = '';
+  enteredText = '';
   add = output<{title: string; text: string}>();
 
   ngOnInit(): void {
@@ -26,9 +29,10 @@ export class NewTicketComponent implements OnInit ,AfterViewInit {
     console.log(this.form?.nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string) {
-    this.add.emit({title: title, text: ticketText});
-    
-    this.form?.nativeElement.reset();
+  onSubmit() {
+    this.add.emit({title: this.enteredTitle, text: this.enteredText});
+    // this.form?.nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
