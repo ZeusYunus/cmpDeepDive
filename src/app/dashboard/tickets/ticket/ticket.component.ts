b4e8c1e2c5402e1a67c9b821b355091a46c69d25
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-07-28 20:56:43 +0200

    revisiting inputs and signals

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { Ticket } from '../tickets.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
